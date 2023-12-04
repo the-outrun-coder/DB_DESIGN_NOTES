a70112d3880fa7f4baa37558877145469bab5dc6
David Pineda <david.pineda.codes@gmail.com>
2023-12-04 06:09:04 -0500

    First pass at planning a CMS project table schema

---
+++ PostgresQL/sessions/23_12_03-04_annex_CMS_v1.sql
@@ -0,0 +1,58 @@
+-- MVP Annex CMS schema v1 Entities
+-- user(s)
+-- project
+-- page
+-- node(s)
+-- asset(s)
+
+CREATE TABLE IF NOT EXISTS user_actual (
+  user_id SERIAL PRIMARY KEY
+  name_first
+  name_last
+  contact_email
+  contact_phone
+  -- socials ?
+  -- 
+);
+CREATE TABLE IF NOT EXISTS user_admin (
+  id SERIAL PRIMARY KEY
+  -- FK => user_actual
+);
+
+CREATE TABLE IF NOT EXISTS user_author (
+  id SERIAL PRIMARY KEY
+  -- FK => user_actual
+);
+CREATE TABLE IF NOT EXISTS user_editor (
+  id SERIAL PRIMARY KEY
+  -- FK => user_actual
+  
+);
+CREATE TABLE IF NOT EXISTS user_visitor (
+  id SERIAL PRIMARY KEY
+  -- FK => user_actual
+);
+
+-- 
+
+CREATE TABLE IF NOT EXISTS preferences (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS project (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS page (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS node_layout (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS node_content (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS asset_graphic (
+  id SERIAL PRIMARY KEY
+);
+CREATE TABLE IF NOT EXISTS asset_media (
+  id SERIAL PRIMARY KEY
+);
